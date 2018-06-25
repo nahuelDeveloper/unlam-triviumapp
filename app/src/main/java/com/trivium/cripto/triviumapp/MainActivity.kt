959adef19da5a1243929f693d9a6b1d80726cebf
rulo <nahuel.developer@gmail.com>
2018-06-24 21:51:19 -0300

    Nahuel - Logica para abrir la galeria y seleccionar una imagen.

--- app/src/main/java/com/trivium/cripto/triviumapp/MainActivity.kt
+++ app/src/main/java/com/trivium/cripto/triviumapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.trivium.cripto.triviumapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT = 1
+
+    var btnPickImage: Button? = null
+    var ivGallery: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.btnPickImage = findViewById<Button>(R.id.btnPickImage)
+        this.btnPickImage?.setOnClickListener {
+            val intent = Intent(
+                    Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            startActivityForResult(intent, RESULT)
+        }
     }
 }
